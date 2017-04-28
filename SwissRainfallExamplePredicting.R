5044105096d305f24afd71ed518bd7597154a691
Rohan Arambepola <scro3122@ndph.ox.ac.uk>
2017-04-28 13:11:38 +0100

    Added points for prediction and plotted the result

--- SwissRainfallExamplePredicting.R
+++ SwissRainfallExamplePredicting.R
@@ -1,5 +1,3 @@
-
-
 library(geoR) #package containing the rainfall data
 library(INLA)
 
@@ -29,18 +27,30 @@ A <- inla.spde.make.A(mesh=Swiss.mesh, loc=coord)
 
 s.index <- inla.spde.make.index(name="spatial.field", n.spde=Swiss.spde$n.spde)
 
+#create prediction stuff
+
+
+point <- rbind(c(-45.38, -70),c(-1, -2),c(-3, -4),c(-5, -6),c(-7, -8),c(-9, -10))
+elevation.pred = c(0.5,1.0,1.5,2.0,2.5,3.0)
+Apred <- inla.spde.make.A(Swiss.mesh, loc=point)
+stack.pred <- inla.stack(data=list(rain=NA), A=list(Apred,1), effects=list(c(s.index, list(Intercept=1)),
+                                                                    list(Elevation=elevation.pred)), tag="pred")
+
+
 #create data stack for normal and poisson examples
 stack.norm <- inla.stack(data=list(rain=datn), A=list(A,1), effects=list(c(s.index, list(Intercept=1)),
                                                                     list(Elevation=elevation)), tag="all")
 
 
 stack.pois <- inla.stack(data=list(rain=datp), A=list(A,1), effects=list(c(s.index, list(Intercept=1)),
-                                                                       list(Elevation=elevation)), tag="all")
+                                                                    list(Elevation=elevation)), tag="all")
 
 #specify linear model
 formula <- rain ~ -1 + Intercept + Elevation + f(spatial.field, model=spde)
 
 #run estimation
+
+if(FALSE){
 Swiss.output.pois <- inla(formula, data=inla.stack.data(stack.pois, spde=Swiss.spde), family="poisson", 
                      control.predictor=list(A=inla.stack.A(stack.pois), compute=TRUE),
                      control.compute=list(cpo=TRUE, dic=TRUE))
@@ -49,18 +59,43 @@ Swiss.output.norm <- inla(formula, data=inla.stack.data(stack.norm, spde=Swiss.s
                          control.predictor=list(A=inla.stack.A(stack.norm), compute=TRUE),
                          control.compute=list(cpo=TRUE, dic=TRUE))
 
-Swiss.output.pois$summary.fixed
+#Swiss.output.pois$summary.fixed
 Swiss.output.norm$summary.fixed
+}
 
-plot(elevation, datn)
+#predicting at the same time
+stack.join <- inla.stack(stack.norm, stack.pred) #normal
+#stack.join <- inla.stack(stack.pois, stack.pred) #poisson
+
+Swiss.output.pred <- inla(formula, data=inla.stack.data(stack.join, spde=Swiss.spde), family="gaussian", 
+                          control.predictor=list(A=inla.stack.A(stack.join), compute=TRUE),
+                          control.compute=list(cpo=TRUE, dic=TRUE))
+
+Swiss.output.pred$summary.fixed
+
+#find index of prediction point and find prediction
+index <- inla.stack.index(stack.join, tag='pred')$data
+round(Swiss.output.pred$summary.linear.pred[index,], 4)
+
+nu.pred = Swiss.output.pred$summary.linear.pred[index,]$mean
+rainfall.pred = array(0,c(length(nu.pred),1))
+for(i in 1:length(nu.pred)){
+  #rainfall.pred[i] = rpois(1, exp(nu.pred[i]))
+  rainfall.pred[i] = rnorm(1, nu.pred[i], 1)
+}
+
+plot(elevation, datn, pch=19)
+#plot(elevation, datp, pch=19)
+points(elevation.pred, rainfall.pred, col = "red", pch=19)
 
 
 
 if(FALSE){
+plot(elevation, datn)
 plot(coord, cex=elevation, main="Elevation")
 #plot(coord, cex=datp/40, main = "Poisson response")
 plot(coord, cex=datn/20, main = "Normal response")
-  
+
 rbPal <- colorRampPalette(c('red','blue'))
 elevation.col <- rbPal(10)[as.numeric(cut(elevation,breaks = 10))]
 plot(coord, col=elevation.col, main="Elevation", pch=19)
